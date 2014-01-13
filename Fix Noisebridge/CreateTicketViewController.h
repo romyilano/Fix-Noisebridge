22a4b9621a171410ed0ff247fc6e552676b2008b
Romy Ilano <romy.ilano@gmail.com>
2014-01-12 20:22:43 -0800

    added missing view controllers

---
+++ Fix Noisebridge/CreateTicketViewController.h
@@ -0,0 +1,16 @@
+//
+//  CreateTicketViewController.h
+//  Fix NB
+//
+//  Created by Romy Ilano on 1/10/14.
+//  Copyright (c) 2014 Romy Ilano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CreateTicketViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *rightBarButton;
+
+
+@end
